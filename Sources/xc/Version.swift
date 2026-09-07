60acea8e4575097d7b35aa9778df86c3f09cffc1
Mathew Gacy <422815+mgacy@users.noreply.github.com>
2026-09-07 14:26:43 -0700

    Bump Version.swift -> 0.1.0 (#3)

--- Sources/xc/Version.swift
+++ Sources/xc/Version.swift
@@ -3,5 +3,5 @@
 /// Namespace for the current version of the target in which this file is contained.
 enum Version {
     /// The current version number.
-    static let number = "0.0.1"
+    static let number = "0.1.0"
 }
